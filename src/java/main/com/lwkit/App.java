262d0855923039d8affb7d61fe9be863470797d8
ldestinywei <lwmczy@gmail.com>
2021-11-12 22:31:57 +0800

    delete src directory

--- src/java/main/com/lwkit/App.java
+++
@@ -1,8 +0,0 @@
-package com.lwkit;
-
-public class App {
-    public static void main(String[] args) {
-        EnumMonkeyKing k1 = EnumMonkeyKing.INSTANCE;
-        System.out.println(k1.toString());
-    }
-}
