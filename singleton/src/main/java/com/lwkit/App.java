e1d38eeaf5cde82fe10e433465e510ec0a9cdaf4
ldestinywei <lwmczy@gmail.com>
2021-11-12 23:03:50 +0800

    add files.

---
+++ singleton/src/main/java/com/lwkit/App.java
@@ -0,0 +1,8 @@
+package com.lwkit;
+
+public class App {
+    public static void main(String[] args) {
+        EnumMonkeyKing e1 = EnumMonkeyKing.INSTANCE;
+        System.out.println(e1.toString());
+    }
+}
---
+++ singleton/src/main/java/com/lwkit/EnumMonkeyKing.java
@@ -0,0 +1,11 @@
+package com.lwkit;
+
+public enum EnumMonkeyKing {
+
+    INSTANCE;
+
+    @Override
+    public String toString() {
+        return getDeclaringClass().getCanonicalName() + '@' + hashCode();
+    }
+}
