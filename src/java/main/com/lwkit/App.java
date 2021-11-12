05ed3582a4752e2114160bbc2654b21de759226f
ldestinywei <lwmczy@gmail.com>
2021-11-12 17:32:39 +0800

    add App and EnumMonkeyKing.

---
+++ src/java/main/com/lwkit/App.java
@@ -0,0 +1,8 @@
+package com.lwkit;
+
+public class App {
+    public static void main(String[] args) {
+        EnumMonkeyKing k1 = EnumMonkeyKing.INSTANCE;
+        System.out.println(k1.toString());
+    }
+}
